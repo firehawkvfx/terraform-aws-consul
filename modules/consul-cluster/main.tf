084a322d27f75ecc1e37494206e2de1ff010c836
Andrew Graham <queglay@gmail.com>
2022-06-12 21:33:46 +0930

    Use plain name of launch config since it includes the prefix

--- modules/consul-cluster/main.tf
+++ modules/consul-cluster/main.tf
@@ -15,7 +15,7 @@ terraform {
 
 resource "aws_autoscaling_group" "autoscaling_group" {
   # name_prefix = var.cluster_name
-  name                 = "${var.cluster_name}-${aws_launch_configuration.launch_configuration.name}"
+  name                 = "${aws_launch_configuration.launch_configuration.name}"
   launch_configuration = aws_launch_configuration.launch_configuration.name
 
   availability_zones  = var.availability_zones
